15b57c07824c46c3d361653db08b50467fcd132a
wetterwebdesign <matthewwetter@Matthews-MBP.home>
2017-08-16 00:23:27 -0400

    Fix for calendar scroll on mobile

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -59,6 +59,11 @@ $(document).ready(function() {
   // DATEPICKER
   $( "#datepicker" ).datepicker();
 
+  $('#datepicker').on('focus', function(e) {
+    e.preventDefault(); e.stopPropagation();
+    window.scrollTo(0,0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px. 
+});
+
 });
 // FULLY REFERENCE JQUERY AFTER THIS
 
