42fdea7579963bfd7336a3dcc8903cebc57eead7
mecreativedesign <mecreativedesigner@gmail.com>
2018-01-02 21:57:52 +0600

    all files for test project

---
+++ js/scripts.js
@@ -0,0 +1,135 @@
+(function ($) {	
+    "use strict";
+	//Angular js script for add licence state START
+	
+	var app=angular.module('myApp',[]);
+
+	app.controller('inspectionController', function ($scope, $http) {
+	$scope.choiceSet = {choices: []};
+    $scope.quest = {};
+
+    $scope.choiceSet.choices = [];
+    $scope.addNewChoice = function () {
+        $scope.choiceSet.choices.push('');
+    };
+
+    $scope.removeChoice = function (z) {
+        //var lastItem = $scope.choiceSet.choices.length - 1;
+        $scope.choiceSet.choices.splice(z,1);
+    };
+    });
+	//Angular js script for add licence state END
+    $(document).ready(function () {
+        /*==Left Navigation Accordion ==*/
+        if ($.fn.dcAccordion) {
+            $('#nav-accordion').dcAccordion({
+                eventType: 'click',
+                autoClose: true,
+                saveState: true,
+                disableLink: true,
+                speed: 'slow',
+                showCount: false,
+                autoExpand: true,
+                classExpand: 'dcjq-current-parent'
+            });
+        }
+       
+        /*==Nice Scroll ==*/
+        if ($.fn.niceScroll) {
+
+
+            $(".leftside-navigation").niceScroll({
+                cursorcolor: "#1FB5AD",
+                cursorborder: "0px solid #fff",
+                cursorborderradius: "0px",
+                cursorwidth: "3px"
+            });
+
+            $(".leftside-navigation").getNiceScroll().resize();
+            if ($('#sidebar').hasClass('hide-left-bar')) {
+                $(".leftside-navigation").getNiceScroll().hide();
+            }
+            $(".leftside-navigation").getNiceScroll().show();
+
+            $(".right-stat-bar").niceScroll({
+                cursorcolor: "#1FB5AD",
+                cursorborder: "0px solid #fff",
+                cursorborderradius: "0px",
+                cursorwidth: "3px"
+            });
+
+        }
+
+     
+        /*==Sidebar Toggle==*/
+
+        $(".leftside-navigation .sub-menu > a").click(function () {
+            var o = ($(this).offset());
+            var diff = 80 - o.top;
+            if (diff > 0)
+                $(".leftside-navigation").scrollTo("-=" + Math.abs(diff), 500);
+            else
+                $(".leftside-navigation").scrollTo("+=" + Math.abs(diff), 500);
+        });
+
+
+
+        $('.sidebar-toggle-box .fa-bars').click(function (e) {
+
+            $(".leftside-navigation").niceScroll({
+                cursorcolor: "#1FB5AD",
+                cursorborder: "0px solid #fff",
+                cursorborderradius: "0px",
+                cursorwidth: "3px"
+            });
+
+            $('#sidebar').toggleClass('hide-left-bar');
+            if ($('#sidebar').hasClass('hide-left-bar')) {
+                $(".leftside-navigation").getNiceScroll().hide();
+            }
+            $(".leftside-navigation").getNiceScroll().show();
+            $('#main-content').toggleClass('merge-left');
+            e.stopPropagation();
+            if ($('#container').hasClass('open-right-panel')) {
+                $('#container').removeClass('open-right-panel')
+            }
+            if ($('.right-sidebar').hasClass('open-right-bar')) {
+                $('.right-sidebar').removeClass('open-right-bar')
+            }
+
+            if ($('.header').hasClass('merge-header')) {
+                $('.header').removeClass('merge-header')
+            }
+
+
+        });
+        $('.toggle-right-box .fa-bars').click(function (e) {
+            $('#container').toggleClass('open-right-panel');
+            $('.right-sidebar').toggleClass('open-right-bar');
+            $('.header').toggleClass('merge-header');
+
+            e.stopPropagation();
+        });
+
+        $('.header,#main-content,#sidebar').click(function () {
+            if ($('#container').hasClass('open-right-panel')) {
+                $('#container').removeClass('open-right-panel')
+            }
+            if ($('.right-sidebar').hasClass('open-right-bar')) {
+                $('.right-sidebar').removeClass('open-right-bar')
+            }
+
+            if ($('.header').hasClass('merge-header')) {
+                $('.header').removeClass('merge-header')
+            }
+
+
+        });
+
+
+      
+
+    });
+
+
+})(jQuery);
